5bd7dbcf34fc41527e69ea6f9eb8841eb52082da
maryane <maryane.castro993@gmail.com>
2023-08-31 12:36:18 -0300

    Update testeweb3.py

--- testeweb3.py
+++ testeweb3.py
@@ -1,4 +1,3 @@
-#usando tecnologia ethereum
 '''
 RODAR O GANACHE
 cd squashfs-root
@@ -12,26 +11,26 @@ from web3 import Web3
 ganache_url = "http://127.0.0.1:7545"
 web3 = Web3(Web3.HTTPProvider(ganache_url))
 
-# Verificar se está conectado à rede
+# verificar conexao da rede
 if web3.is_connected():
     print("Conectado à rede Ethereum")
 else:
     print("Não foi possível conectar à rede Ethereum")
 
-# Exemplo de endereço da conta (mude para um endereço real ou crie uma nova usando Ganache)
-account_address = "0xd4f934BB55b450f25f7cb2BD236d85C702549A7D"
+# conta de quem deposita
+account_address = "0xa8f7595Dea4ABC916e69f86Ce0760A3a3DA9550d"
 
-# Exemplo de chave privada da conta (usado apenas para fins educacionais, NÃO COMPARTILHE)
-private_key = "0x04b303694c98f2dc1e5ab45e4be3bf7c8753df82f8e98bc34d5fe5d78bae04df"
+# chave de quem deposita
+private_key = "0x0db874ae678c60b088da789a57594b81aadfeab369a0d10aa36e72d4f23e9112"
 
 # Obter o saldo da conta
 balance_wei = web3.eth.get_balance(account_address)
 balance_eth = web3.from_wei(balance_wei, "ether")
 print(f"Saldo da conta: {balance_eth} ETH")
 
-# Enviar uma transação (usado apenas para fins educacionais, NÃO COMPARTILHE)
+# transação (usado apenas para fins educacionais, NÃO COMPARTILHE)
 transaction = {
-    "to": "0xa8f7595Dea4ABC916e69f86Ce0760A3a3DA9550d",
+    "to": "0xd4f934BB55b450f25f7cb2BD236d85C702549A7D",
     "value": web3.to_wei(0.3, "ether"),
     "gas": 2000000,
     "gasPrice": web3.to_wei("50", "gwei"),
